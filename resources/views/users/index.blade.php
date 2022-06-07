d0771efaa3e7db511ac6ad36159679cf607bc7fd
franklinperseuDS <fpdll.cid20@uea.edu.br>
2022-06-07 15:11:12 -0400

    iniciando o tailwind

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -3,23 +3,46 @@
 @section('title', 'Listagem dos usuarios')
 
 @section('content')
-<h1>Listagem de Usuarios
-    (<a href="{{ route('users.create')}}">+ </a>)
+<h1 class="text-2xl font-semibold leading-tigh py-2">
+    Listagem de Usuarios
+    <a href="{{ route('users.create')}}" class="bg-blue-900 rounded-full"> (+) </a>
 </h1>
 
 <form action="{{ route('users.index') }}" method="get">
     <input type="text" name="search" placeholder="Pesquisar">
     <button>Pesquisar</button>
 </form>
+<table class="min-w-full leading-normal shadow-md rounded">
+    <thead>
+        <tr>
+            <th>Nome</th>
+            <th>E-mail</th>
+            <th>Editar</th>
+            <th>Detalhes</th>
+        </tr>
+    </thead>
 
-<ul>
-@foreach ($users as $user)
-    <li>
-    {{ $user->name}} - 
-    {{ $user->email}} |
-    <a href="{{ route('users.edit', $user->id)}}">Editar </a> | 
+    <tbody>
+        @foreach ($users as $user)
+    <tr>
+        <td>
+    {{ $user->name}} 
+        </td>
+        <td>
+    {{ $user->email}} 
+        </td>
+        <td>
+    <a href="{{ route('users.edit', $user->id)}}">Editar </a> 
+        </td>
+        <td>
     <a href="{{ route('users.show', $user->id)}}">Detalhes </a>
-    </li>
+        </td>
+    </tr>
+    
 @endforeach
-</ul>
+    </tbody>
+</table>
+
+
+
 @endsection
