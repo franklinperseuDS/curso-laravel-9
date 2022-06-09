ce00566da1c15dc41e11ec19edab48a6f1e903ae
franklinperseuDS <fpdll.cid20@uea.edu.br>
2022-06-09 14:36:54 -0400

    alterações no front

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -5,41 +5,40 @@
 @section('content')
 <h1 class="text-2xl font-semibold leading-tigh py-2">
     Listagem de Usuarios
-    <a href="{{ route('users.create')}}" class="bg-blue-900 rounded-full"> (+) </a>
+    <a href="{{ route('users.create')}}" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-3 py-1.5 text-center dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800"> + </a>
 </h1>
 
 <form action="{{ route('users.index') }}" method="get">
     <input type="text" name="search" placeholder="Pesquisar">
     <button>Pesquisar</button>
 </form>
-<table class="min-w-full leading-normal shadow-md rounded">
-    <thead>
+<table class="min-w-full text-left leading-normal shadow-md rounded">
+    <thead class="text-xs text-gray-700 uppercase bg-gray-50 ">
         <tr>
-            <th>Nome</th>
-            <th>E-mail</th>
-            <th>Editar</th>
-            <th>Detalhes</th>
+            <th scope="col" class="px-6 py-3">Nome</th>
+            <th scope="col" class="px-6 py-3">E-mail</th>
+            <th scope="col" class="px-6 py-3">Editar</th>
+            <th scope="col" class="px-6 py-3">Detalhes</th>
         </tr>
     </thead>
 
     <tbody>
-        @foreach ($users as $user)
-    <tr>
-        <td>
-    {{ $user->name}} 
-        </td>
-        <td>
-    {{ $user->email}} 
-        </td>
-        <td>
-    <a href="{{ route('users.edit', $user->id)}}">Editar </a> 
-        </td>
-        <td>
-    <a href="{{ route('users.show', $user->id)}}">Detalhes </a>
-        </td>
-    </tr>
-    
-@endforeach
+    @foreach ($users as $user)
+        <tr class="bg-white border-b px-6 py-4">
+            <td class="px-6 py-4">
+                 {{ $user->name}} 
+            </td>
+            <td class="px-6 py-4">
+                 {{ $user->email}} 
+            </td>
+            <td class="px-6 py-4">
+                 <a class="bg-yellow-700 rounded text-white px-3 py-1.5" href="{{ route('users.edit', $user->id)}}">Editar </a> 
+            </td>
+            <td class="px-6 py-4">
+                <a class="bg-red-700 rounded text-white px-3 py-1.5" href="{{ route('users.show', $user->id)}}">Detalhes </a>
+            </td>
+        </tr>
+    @endforeach
     </tbody>
 </table>
 
--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -2,15 +2,21 @@
 
 @section('title', 'Listagem do usuario')
 @section('content')
-<h1>Listagem do Usuario  {{$user->name}}</h1>
+<a href="{{ url()->previous() }}" class="bg-blue-700 rounded text-white font-sans px-3 py-1.5"> Voltar</a>
+<h1 class="text-2xl font-semibold leading-tigh py-2">
+    Listagem do Usuario  {{$user->name}}</h1>
 
-<ul>
-    <li> {{ $user->name}} </li>
-    <li> {{ $user->email}} </li>
-</ul>
-<form action=" {{ route('users.destroy', $user->id)}}" method="POST">
-    @method('DELETE')
-    @csrf
-<button> Delete</button>
-</form>
+
+    
+    <div class="mb-6 flex flex-row font-sans shadow-md">
+    <div class="basis-1/2">NOME: {{ $user->name}} </div>
+    <div class="basis-1/2">E-MAIL: {{ $user->email}} </div>
+    <div class="basis-1/2">
+    <form action=" {{ route('users.destroy', $user->id)}}" method="POST">
+        @method('DELETE')
+        @csrf
+    <button class="bg-red-700 rounded text-white px-3 py-1.5"> Delete</button>
+    </form>
+    </div>
+</div>
 @endsection
